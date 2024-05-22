07c3c37fcdccb6c2768b08b000b4230c5c566957
m-ji97 <tv5035@naver.com>
2024-05-22 13:08:02 +0900

    mj

--- src/main/java/com/javaex/controller/MjController.java
+++ src/main/java/com/javaex/controller/MjController.java
@@ -3,10 +3,14 @@ package com.javaex.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.javaex.service.MjService;
 import com.javaex.vo.MjVo;
 
+@RestController
 public class MjController {
 	
 	//필드
@@ -22,6 +26,22 @@ public class MjController {
 		return MjList;
 	}
 	
+	//등록
+	public int wirte(@RequestBody MjVo MjVo) {
+		System.out.println("MjController.write");
+		
+		int count = mjService.exeWrite(MjVo);
+		
+		return count;
+	}
 	
+	//삭제
+	public int delete(@RequestParam(value="personId")int no) {
+		System.out.println("MjController.delete");
+		
+		int count = mjService.exeDelete(no);
+		
+		return count;
+	}
 
 }
