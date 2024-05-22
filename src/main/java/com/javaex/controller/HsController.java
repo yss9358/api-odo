fc70af07c8bb0e400e552633270935c7271c7d0e
rhs0402 <rhs0402@naver.com>
2024-05-22 17:56:45 +0900

    hs

--- src/main/java/com/javaex/controller/HsController.java
+++ src/main/java/com/javaex/controller/HsController.java
@@ -9,16 +9,16 @@ import com.javaex.service.HsService;
 @RestController
 public class HsController {
 
-	@Autowired
-	private HsService hsService;
-
-	// 메인 페이지 리스트
-	@GetMapping("odo/mains")
-	public void getList() {
-		System.out.println("HsController.getList()");
-
-		hsService.exegetList();
-
-	}
+//	@Autowired
+//	private HsService hsService;
+//
+//	// 메인 페이지 리스트
+//	@GetMapping("odo/mains")
+//	public void getList() {
+//		System.out.println("HsController.getList()");
+//
+//		hsService.exegetList();
+//
+//	}
 
 }
--- src/main/java/com/javaex/service/HsService.java
+++ src/main/java/com/javaex/service/HsService.java
@@ -8,26 +8,26 @@ import com.javaex.dao.HsDao;
 @Service
 public class HsService {
 
-	@Autowired
-	private HsDao hsDao;
-	
-	//메인페이지 리스트
-	public void exegetList() {
-		System.out.println("HsService.exegetList()");
-		
-		//무료 베스트 클래스 리스트
-		hsDao.selectFreeBestList();
-		
-		//유료 베스트 클래스 리스트
-		hsDao.selectPayBestList();
-		
-		//신규 베스트 클래스 리스트
-		hsDao.selectNewBestList();
-		
-		//리스트의 리스트
-		//List<List<HsVo>> listOfLists
-		//listOfLists.add(무료베스트클래스리스트)
-		//listOfLists.add(유료베스트클래스리스트)
-		//listOfLists.add(신규베스트클래스리스트)
-	}
+//	@Autowired
+//	private HsDao hsDao;
+//	
+//	//메인페이지 리스트
+//	public void exegetList() {
+//		System.out.println("HsService.exegetList()");
+//		
+//		//무료 베스트 클래스 리스트
+//		hsDao.selectFreeBestList();
+//		
+//		//유료 베스트 클래스 리스트
+//		hsDao.selectPayBestList();
+//		
+//		//신규 베스트 클래스 리스트
+//		hsDao.selectNewBestList();
+//		
+//		//리스트의 리스트
+//		//List<List<HsVo>> listOfLists
+//		//listOfLists.add(무료베스트클래스리스트)
+//		//listOfLists.add(유료베스트클래스리스트)
+//		//listOfLists.add(신규베스트클래스리스트)
+//	}
 }
