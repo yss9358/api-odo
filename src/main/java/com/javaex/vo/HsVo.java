3ea7ee9000200fd9488aef5f337d906ae8399715
rhs0402 <rhs0402@naver.com>
2024-05-22 16:58:59 +0900

    hs

---
+++ src/main/java/com/javaex/vo/HsVo.java
@@ -0,0 +1,40 @@
+package com.javaex.vo;
+
+public class HsVo {
+
+	//회사 필드
+	private int comNo;
+	private String comName;
+	private String comImg;
+	private String comNameAdd;
+	private String comNumAdd;
+	private String comDetailAdd;
+	private String latitude;
+	private String longtitude;
+	
+	//1차카테고리필드
+	private int cate1No;
+	private String cate1Name;
+	
+	//2차카테고리필드
+	private int cate2No;
+	private String cate2Name;
+	
+	//클래스 필드
+	private int classNo;
+	private String className;
+	private int classType;
+	private String classIntro;
+	private String classNameAdd;
+	private String classNumAdd;
+	private String classDetailAdd;
+	private String classLatitude;
+	private String classLongtitude;
+	private String classImg;
+	private int classPrice;
+	private int classMin;
+	private int classMax;
+	private String classInfo;
+	
+	
+}
