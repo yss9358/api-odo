5f2875d23c1129a5ef62f03a41490e900d61817b
m-ji97 <tv5035@naver.com>
2024-05-23 13:23:06 +0900

    mj

--- src/main/java/com/javaex/service/MjService.java
+++ src/main/java/com/javaex/service/MjService.java
@@ -1,6 +1,5 @@
 package com.javaex.service;
 
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,33 +14,6 @@ public class MjService {
 	//필드
 	@Autowired
 	private MjDao mjDao;
-	
-	//전체 리스트
-	public List<MjVo> exeList(){
-		System.out.println("MjService.exeList");
-		
-		List<MjVo> MjList = mjDao.selectList();
-		
-		return MjList;
-	}
-	
-	//등록
-	public int exeWrite(MjVo mjVo){
-		System.out.println("MjService.exeWrite");
-		
-		int count = mjDao.personInsert(mjVo);
-		
-		return count;
-	}
-	
-	//삭제
-	public int exeDelete(int no) {
-		System.out.println("MjService.exeDelete");
-		
-		int count = mjDao.personDelete(no);
-		
-		return count;
-	}
 	
 	//수정폼
 	public Map<String, Object> exeMform(int no) {
--- src/main/java/com/javaex/vo/MjVo.java
+++ src/main/java/com/javaex/vo/MjVo.java
@@ -3,62 +3,73 @@ package com.javaex.vo;
 public class MjVo {
 	
 	//필드
-	private int personId;
-	private String name;
-	private String hp;
-	private String company;
+	int user_no;
+	String user_nickname;
+	String user_hp;
+	String user_birth;
+	String user_pw;
 	
 	
 	//생성자
 	public MjVo() {
 		super();
 	}
+
+	public MjVo(int user_no, String user_nickname, String user_hp, String user_birth, String user_pw) {
+		super();
+		this.user_no = user_no;
+		this.user_nickname = user_nickname;
+		this.user_hp = user_hp;
+		this.user_birth = user_birth;
+		this.user_pw = user_pw;
+	}
 	
 	//메소드-gs
-	public MjVo(int personId, String name, String hp, String company) {
-		super();
-		this.personId = personId;
-		this.name = name;
-		this.hp = hp;
-		this.company = company;
+	public int getUser_no() {
+		return user_no;
 	}
 
-	public int getPersonId() {
-		return personId;
+	public void setUser_no(int user_no) {
+		this.user_no = user_no;
 	}
 
-	public void setPersonId(int personId) {
-		this.personId = personId;
+	public String getUser_nickname() {
+		return user_nickname;
 	}
 
-	public String getName() {
-		return name;
+	public void setUser_nickname(String user_nickname) {
+		this.user_nickname = user_nickname;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public String getUser_hp() {
+		return user_hp;
 	}
 
-	public String getHp() {
-		return hp;
+	public void setUser_hp(String user_hp) {
+		this.user_hp = user_hp;
 	}
 
-	public void setHp(String hp) {
-		this.hp = hp;
+	public String getUser_birth() {
+		return user_birth;
 	}
 
-	public String getCompany() {
-		return company;
+	public void setUser_birth(String user_birth) {
+		this.user_birth = user_birth;
 	}
 
-	public void setCompany(String company) {
-		this.company = company;
+	public String getUser_pw() {
+		return user_pw;
+	}
+
+	public void setUser_pw(String user_pw) {
+		this.user_pw = user_pw;
 	}
 	
 	//메소드-일반
 	@Override
 	public String toString() {
-		return "MjVo [personId=" + personId + ", name=" + name + ", hp=" + hp + ", company=" + company + "]";
+		return "MjVo [user_no=" + user_no + ", user_nickname=" + user_nickname + ", user_hp=" + user_hp
+				+ ", user_birth=" + user_birth + ", user_pw=" + user_pw + "]";
 	}
 
 }
