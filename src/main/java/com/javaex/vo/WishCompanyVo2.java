564509f3b510ccc22cc502fea9c81f32dbcadde0
hyeon0714 <xodid6960@naver.com>
2024-05-23 16:47:54 +0900

    ㅎㅎㅎ

---
+++ src/main/java/com/javaex/vo/WishCompanyVo2.java
@@ -0,0 +1,69 @@
+package com.javaex.vo;
+
+public class WishCompanyVo2 {
+
+	private String companyImage;
+	private String companyName;
+	private int classCount;
+	private int reviewCount;
+	private int wishCount;
+	
+	public WishCompanyVo2() {
+		super();
+	}
+
+	public WishCompanyVo2(String companyImage, String companyName, int classCount, int reviewCount, int wishCount) {
+		super();
+		this.companyImage = companyImage;
+		this.companyName = companyName;
+		this.classCount = classCount;
+		this.reviewCount = reviewCount;
+		this.wishCount = wishCount;
+	}
+
+	public String getCompanyImage() {
+		return companyImage;
+	}
+
+	public void setCompanyImage(String companyImage) {
+		this.companyImage = companyImage;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public int getClassCount() {
+		return classCount;
+	}
+
+	public void setClassCount(int classCount) {
+		this.classCount = classCount;
+	}
+
+	public int getReviewCount() {
+		return reviewCount;
+	}
+
+	public void setReviewCount(int reviewCount) {
+		this.reviewCount = reviewCount;
+	}
+
+	public int getWishCount() {
+		return wishCount;
+	}
+
+	public void setWishCount(int wishCount) {
+		this.wishCount = wishCount;
+	}
+
+	@Override
+	public String toString() {
+		return "WishCompanyVo2 [companyImage=" + companyImage + ", companyName=" + companyName + ", classCount="
+				+ classCount + ", reviewCount=" + reviewCount + ", wishCount=" + wishCount + "]";
+	}
+}
