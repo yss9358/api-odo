feb4ed99179432814954e17c396f2c0144b515c2
m-ji97 <tv5035@naver.com>
2024-06-04 17:53:14 +0900

    MJ

--- src/main/java/com/javaex/controller/MjController.java
+++ src/main/java/com/javaex/controller/MjController.java
@@ -20,89 +20,86 @@ import com.javaex.vo.MjVo;
 
 import jakarta.servlet.http.HttpServletRequest;
 
-
 @RestController
 public class MjController {
-	
-	//필드
+
+	// 필드
 	@Autowired
 	private MjService mjService;
-	
-	//수정폼
+
+	// 수정폼
 	@GetMapping("/odo/mypage/modify/{no}")
-	public Map<String, Object> modifyform(@PathVariable(value="no")int no) {
+	public Map<String, Object> modifyform(@PathVariable(value = "no") int no) {
 		System.out.println("MjController.modifyform");
-		
+
 		Map<String, Object> pMap = mjService.exeMform(no);
-		
+
 		return pMap;
 	}
-	
-	//수정
+
+	// 수정
 	@PostMapping("/odo/mypage/modify")
 	public int modify(@RequestBody MjVo mjVo) {
 		System.out.println("MjController.modify");
-		
-		int count = 0; 
-				
+
+		int count = 0;
+
 		try {
-			//1. 먼저 select해서 기존 데이터 체크함
-			//2. 데이터 없으면 에러처리
-			//3. 데이터 있으면 기존로직 수행
-			
+			// 1. 먼저 select해서 기존 데이터 체크함
+			// 2. 데이터 없으면 에러처리
+			// 3. 데이터 있으면 기존로직 수행
+
 			count = mjService.exeModify(mjVo);
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
+
 		return count;
 	}
-	
-	//리스트
+
+	// 리스트
 	@GetMapping("/odo/mypage/notice")
 	public Map<String, Object> list(HttpServletRequest request, Model model) {
 		System.out.println("MjController.list");
 		int no = JwtUtil.getNoFromHeader(request);
-			
+
 		List<MjVo> MjList = mjService.exeList();
-			
+
 		model.addAttribute("list", MjList);
-		
+
 		Map<String, Object> Mlist = new HashMap<>();
-		
+
 		Mlist.put("no", no);
 		Mlist.put("MjList", MjList);
-			
+
 		return Mlist;
 	}
-	
-	//읽기
+
+	// 읽기
 	@GetMapping("/odo/mypage/notice/{no}")
-	public Map<String, Object> Read(HttpServletRequest request,@PathVariable("no") int no) {
+	public Map<String, Object> Read(HttpServletRequest request, @PathVariable("no") int no) {
 		int num = JwtUtil.getNoFromHeader(request);
-		
+
 		MjVo MjVo = mjService.exeRead(no);
-		
+
 		Map<String, Object> Mread = new HashMap<>();
-		
+
 		Mread.put("num", num);
 		Mread.put("MjVo", MjVo);
-		
+
 		return Mread;
 	}
-	
-	//등록폼
-	
-	
-	//등록
+
+	// 등록폼
+
+	// 등록
 	@PostMapping("/odo/mypage/notice/write")
 	public String write(HttpServletRequest request, @RequestBody MjVo mjVo) {
 //		System.out.println("MjController.write()");
 		int num = JwtUtil.getNoFromHeader(request);
 		mjVo.setUserno(num);
 		System.out.println(mjVo);
-		
+
 //		
 		int count = mjService.exeWrite(mjVo);
 //		
@@ -110,29 +107,27 @@ public class MjController {
 //		
 		return result;
 	}
-	
-	//삭제
+
+	// 삭제
 	@DeleteMapping("/odo/mypage/notice/delete/{no}")
 	public String delete(@PathVariable("no") int no) {
 		System.out.println("MjController.delete");
-		
+
 		int count = mjService.exeDelete(no);
-		
+
 		String result = count + "건 삭제 되었습니다.";
-		
+
 		return result;
 	}
-	
-	//통계리스트
-	//public void 
+
+	// 통계리스트
+	// public void
 	@GetMapping("/odo/chart")
 	public Map<String, Object> chart(HttpServletRequest request) {
 		int no = JwtUtil.getNoFromHeader(request);
 		System.out.println(no);
 		return mjService.exechart(no);
-		
-		
+
 	}
-	
 
 }
